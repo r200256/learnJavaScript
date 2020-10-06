962c46bcb19396319a3457df7eef635eb96817d9
r200256 <r200256@gmail.com>
2020-10-06 17:27:50 +0600

    Day 1-1

--- patn/to/script.js
+++ patn/to/script.js
@@ -1,3 +1,83 @@
-alert("Сейчас будет ошибка");
-[(1, 2)].forEach(alert); //1 ,2
-alert("Я JavaScript");
+https://learn.javascript.ru/type-conversions
+
+// let value = true;
+// console.log(typeof value);
+
+// value = String(value);
+// console.log(typeof value);
+
+// val = prompt("Вопрос", "Ответ");
+// console.log(val);
+
+//true or false
+// let isBoss = confirm("Ты здесь главный?");
+// console.log(isBoss);
+
+//null или второй аргумет
+// val = prompt("Ogo", "12345");
+// console.log(val);
+
+// let name = "Ilya";
+// console.log(`hello ${1}`);
+// console.log(`hello ${"name"}`);
+// console.log(`hello ${name}`);
+
+// console.log(typeof undefined);
+// console.log(typeof 0);
+// console.log(typeof 10n);
+// console.log(typeof true);
+// console.log(typeof "true");
+// console.log(typeof Math);
+// console.log(typeof null);
+// console.log(typeof Symbol("id"));
+// console.log(typeof alert);
+
+// undefined Оно означает, что «значение не было присвоено».
+
+// null Это просто специальное значение, которое представляет собой «ничего», «пусто» или «значение неизвестно».
+
+// let str = "Privet";
+// let str2 = "Privet";
+// let str3 = `Hello ${str}`;
+// let str4 = `результат: ${1 + 2}`;
+
+// const bigInt = 1234567890123456789012345678901234567890n;
+
+//2*53-1 max BigInt
+
+// const bigInt = 123n;
+// console.log(bigInt);
+
+//number Infinity -Infinity NaN - ""/число
+//динамическая типизация
+// let message = "hello";
+// message = 123456;
+
+// const BRITHDAY = "18.04.1930";
+// const age = someCode(BRITHDAY);
+
+// let eart, curentUser;
+
+// let admin, name;
+// name = "Джон";
+// admin = name;
+// console.log(admin);
+// const myDate = "15.15.15";
+// // myDate = "123";
+// console.log(myDate);
+// "use strict";
+// nom = "nom";
+// console.log(nom);
+// let _ = "_",
+//   $ = "$";
+// console.log($);
+// console.log(_);
+// let user = "John",
+//   age = 25,
+//   mes = "Hello";
+// let message;
+// message = "Hello";
+// console.log(message);
+// alert("Сейчас будет ошибка");
+// [(1, 2)].forEach(alert); //1 ,2
+// alert("Я JavaScript");
