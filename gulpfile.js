f4a9a74b3151ac8fed0b12718d1ae4b00e2ffdf0
Home <home@home.xx>
2020-10-20 22:19:27 +0600

    Day2-3

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 const gulp = require('gulp');
+const babel = require('gulp-babel');
 // Зависимости Gulp будут здесь
 gulp.task('default',function(){
     // Задачи Gulp будут здесь
+    // Исходный код для Node
+    gulp.src("es6/**/*.js")
+        .pipe(babel())
+        .pipe(gulp.dest("dist"))
+    // исходный код для браузера
+    gulp.src("public/es6/**/*.js")    
+        .pipe(babel())
+        .pipe(gulp.dest("public/dist"))
 });
