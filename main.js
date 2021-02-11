f09ef02855b378f0c6b4341e38f85663ee4b0120
WORK <work@work>
2021-02-11 16:45:31 +0600

    Day 1 Vue

--- main.js
+++ main.js
@@ -1,3 +1,47 @@
+var app6=new Vue({
+    el:'#app-6',
+    data:{
+        message:'Привет, Vue!!'
+    }
+})
+var app5=new Vue({
+    el:'#app-5',
+    data:{
+        message:'Привет, Vue.js!'
+    },
+    methods:{
+        reverseMessage:function(){
+            this.message=this.message.split('').reverse().join('')
+        }
+    }
+})
+
+var app4=new Vue({
+    el:'#app-4',
+    data:{
+        todos:[
+            {text:'Изучить JavaScript'},
+            {text:'Изучить Vue'},
+            {text:'Создать что нибудь класное'},
+            {text:'JavaScript'},
+            {text:'JavaScript'}
+        ]
+    }
+})
+var app3=new Vue({
+    el:'#app-3',
+    data:{
+        seen:true
+    }
+})
+
+var app2=new Vue({
+    el:"#app-2",
+    data:{
+        message:"DATE --> "+new Date().toLocaleString()
+    }
+})
+
 var appq= new Vue({
     el:'#app',
     data:{
